b57ae8b7593234fcba284741c7531bd18ceaa2b7
Rakib544 <md.rakib10122003@gmail.com>
2021-06-12 13:07:42 +0600

    number, string, array, function. object done

---
+++ src/script.ts
@@ -0,0 +1,47 @@
+// console.log('hello world! I am from Bangladesh')
+
+//declare a variable using typescript
+
+const userName:string = 'Rakib';
+const roll:number = 34;
+//if we try add a string in the second variable, then it will throw an error.
+// const roll:number = '34'; throw an error before compiling
+
+
+
+//declare a function with typescript
+
+const add =(num1:number, num2:number) => {
+    return num1 + num2;
+}
+
+console.log(add(10, 20));
+//if we try to pass a a string when we can call the function, then it will
+//throw an error. Because we explicitly set the parameters will only take number
+// console.log(add('10', 20)); //throw an error before compiling
+
+
+//array
+
+const fruits = ['apple, mango'];
+fruits.push('banana'); //it will added in our array.
+
+//but if we try to add a number inside this array. Then it will throw an error before compiling
+// fruits.push(10); //throw an error because here typescript thinks that this is ana array of string
+
+//But if we make an empty array, then we can push anythings inside the array
+const newArray = [];
+newArray.push(10)
+newArray.push('rakib');
+
+
+// object
+
+const userInfo = {
+    name: 'Rakib',
+    roll: 457100,
+    age: 20
+}
+
+//here userInfo.name=string. But if we try to add any other type here then it will throw an error.
+// userInfo.name = 20; //throw an error because userInfo.name is explicitly set as a string.
