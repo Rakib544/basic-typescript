8d15b1a24f31968b103b7a7393894a3d41f4676c
Rakib544 <md.rakib10122003@gmail.com>
2021-06-12 15:34:37 +0600

    some code uploaded

--- src/script.ts
+++ src/script.ts
@@ -2,8 +2,8 @@
 
 //declare a variable using typescript
 
-const userName:string = 'Rakib';
-const roll:number = 34;
+const userName: string = 'Rakib';
+const roll: number = 34;
 //if we try add a string in the second variable, then it will throw an error.
 // const roll:number = '34'; throw an error before compiling
 
@@ -11,7 +11,7 @@ const roll:number = 34;
 
 //declare a function with typescript
 
-const add =(num1:number, num2:number) => {
+const add = (num1: number, num2: number) => {
     return num1 + num2;
 }
 
@@ -45,3 +45,31 @@ const userInfo = {
 
 //here userInfo.name=string. But if we try to add any other type here then it will throw an error.
 // userInfo.name = 20; //throw an error because userInfo.name is explicitly set as a string.
+
+// if we try to set another property in this object then it will throw an error because
+// it typescript that this object is a schema of object. And that's why we can't set other property to interface.
+
+
+
+//explicit type in typescript
+
+const schoolName: string = 'Barmi Union High School'; // here we explicitly set that this variable can only takes string, but it can't take other values.
+const num: number = 4; //It only takes number value. 
+
+let a: string[] = []; // That means it is an array of string. We can't store other value into it. We can only store string.
+let b: number[] = []; //That means it is an array of number.
+
+let c: (number | string)[] = []; //That means it is an array of number or string.
+let d: string | number; // That means it is an variable of number or string.
+
+let e : {
+    name: string,
+    age: number,
+    isStudent: true
+}
+
+e = {
+    name: 'Rakib',
+    age: 20,
+    isStudent: true
+}; // here I can't set other property in this object.
